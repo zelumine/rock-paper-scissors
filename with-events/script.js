11405f2b0b4c1618c492e0909ffdf9283b2e5fc6
Melusine <zelumine@live.fr>
2019-12-13 22:45:14 +0100

    Update the UI

--- with-events/script.js
+++ with-events/script.js
@@ -4,7 +4,7 @@ let computerScore = 0;
 let computerSelection;
 let playerSelection;
 
-const buttons = document.querySelectorAll("button");
+const buttons = Array.from(document.querySelectorAll("div.button"));
 let moves = document.getElementById("moves");
 let roundResult = document.createElement("p");
 let scores = document.getElementById("score");
@@ -12,81 +12,79 @@ let result = document.getElementById("result");
 
 
 function computerPlay() {
-    let compNumber = Math.floor(Math.random() * 3);
+  let compNumber = Math.floor(Math.random() * 3);
 
-    if(compNumber === 0) {
-        compMove = "Rock";
-    } else if (compNumber === 1) {
-        compMove = "Paper";
-    } else {
-        compMove = "Scissors";
-    }
+  if(compNumber === 0) {
+    compMove = "Rock";
+  } else if (compNumber === 1) {
+    compMove = "Paper";
+  } else {
+    compMove = "Scissors";
+  }
 }    
 
-computerPlay();
-computerSelection = compMove;
-
 function playRound(playerSelection, computerSelection) {
-    if ((playerSelection === "Rock" && computerSelection === "Scissors") || 
-            (playerSelection === "Scissors" && computerSelection === "Paper") || 
-            (playerSelection === "Paper" && computerSelection === "Rock")) {
-        playerScore++;
-        roundResult.textContent = `${playerSelection} beats ${computerSelection}. You win, congrats!`;
-    } else if ((playerSelection === "Scissors" && computerSelection === "Rock") || 
-            (playerSelection === "Paper" && computerSelection === "Scissors") || 
-            (playerSelection === "Rock" && computerSelection === "Paper")) {
-        computerScore++;
-        roundResult.textContent = `${computerSelection} beats ${playerSelection}... You lose!`;
-    } else if ((playerSelection === "Scissors" && computerSelection === "Scissors") || 
+  if ((playerSelection === "Rock" && computerSelection === "Scissors") || 
+      (playerSelection === "Scissors" && computerSelection === "Paper") || 
+      (playerSelection === "Paper" && computerSelection === "Rock")) {
+      playerScore++;
+      roundResult.textContent = `${playerSelection} beats ${computerSelection}. You win, congrats!`;
+  } else if ((playerSelection === "Scissors" && computerSelection === "Rock") || 
+             (playerSelection === "Paper" && computerSelection === "Scissors") || 
+             (playerSelection === "Rock" && computerSelection === "Paper")) {
+      computerScore++;
+      roundResult.textContent = `${computerSelection} beats ${playerSelection}... You lose!`;
+  } else if ((playerSelection === "Scissors" && computerSelection === "Scissors") || 
             (playerSelection === "Rock" && computerSelection === "Rock") || 
             (playerSelection === "Paper" && computerSelection === "Paper")) {
-        computerScore += 0;
-        playerScore += 0;
-        roundResult.textContent = "Oops! It's a tie!";
-    }
+      computerScore += 0;
+      playerScore += 0;
+      roundResult.textContent = "Oops! It's a tie!";
+  }
 }
 
 function game() {
-    computerPlay();
-    playRound(playerSelection, computerSelection);
-        moves.textContent = `Computer plays:  ${compMove}`;
-        moves.appendChild(roundResult);
-        scores.textContent = `Player: ${playerScore} - ${computerScore} : Computer`;    
+  computerPlay();
+  computerSelection = compMove;
+  playRound(playerSelection, computerSelection);
+  moves.textContent = `Computer plays:  ${compMove}`;
+  moves.appendChild(roundResult);
+  scores.textContent = `Player: ${playerScore} - ${computerScore} : Computer`;    
 
-    if(playerScore === 5 || computerScore === 5) {
-        if(playerScore > computerScore) {
-            result.textContent = `You have ${playerScore} points and computer has ${computerScore} points. Congrats, you win!`;
-            result.style.color = 'green';
-            setTimeout( () => {
-                result.textContent = "";
-            }, 1500);
-        } else {
-            result.textContent = `You have ${playerScore} points and computer has ${computerScore} points. Sorry buddy, you lose.`;
-            result.style.color = 'red';
-            setTimeout( () => {
-                result.textContent = "";
-            }, 1500);
-        }
-        playerScore = 0;
-        computerScore = 0;
-        setTimeout( () => {
-        moves.textContent = "Another round?!";
-        }, 1500);
-        scores.textContent = "";
+  if(playerScore === 5 || computerScore === 5) {
+    if(playerScore > computerScore) {
+      result.textContent = `You have ${playerScore} points and computer has ${computerScore} points. Congrats, you win!`;
+      result.style.color = 'green';
+      setTimeout( () => {
+          result.textContent = "";
+      }, 1500);
+    } else {
+      result.textContent = `You have ${playerScore} points and computer has ${computerScore} points. Sorry buddy, you lose.`;
+      result.style.color = 'red';
+      setTimeout( () => {
+          result.textContent = "";
+      }, 1500);
     }
+    playerScore = 0;
+    computerScore = 0;
+    setTimeout( () => {
+      moves.textContent = "Another round?!";
+    }, 1500);
+    scores.textContent = "";
+  }
 }
 
 function playerPlay(e) {
-    if(e.target.id === "rock") {
-        playerSelection = "Rock";
-    } else if(e.target.id === "paper") {
-        playerSelection = "Paper";
-    } else if(e.target.id === "scissors") {
-        playerSelection = "Scissors";
-    }
-    game();
+  if(e.target.id === "rock") {
+    playerSelection = "Rock";
+  } else if(e.target.id === "paper") {
+    playerSelection = "Paper";
+  } else if(e.target.id === "scissors") {
+    playerSelection = "Scissors";
+  }
+  game();
 }
 
 buttons.forEach((button) => {
-    button.addEventListener('click', playerPlay);
+  button.addEventListener('click', playerPlay);
 });
